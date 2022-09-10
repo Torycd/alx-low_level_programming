3bb10a23388169288b6b6021e666f62e04f9c3cb
victory <vicidundun@gmail.com>
2022-09-09 17:42:12 -0700

    hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
- */
+ * Return: 1 if (Success).
+ **/
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
