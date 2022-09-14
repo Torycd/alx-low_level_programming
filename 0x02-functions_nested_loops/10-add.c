4391bb250119be968b4a5e0b20e81e5b2d9913cb
victory <vicidundun@gmail.com>
2022-09-13 19:58:41 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - check main
+ * @a: An integer a
+ * @b: An inetger b
+ * Description: Function that adds two integers and return the result
+ * Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
