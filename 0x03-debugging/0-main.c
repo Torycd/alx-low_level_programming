c039e36630563d6b9e56a6b676c3edc2e6a04033
victory <vicidundun@gmail.com>
2022-09-15 19:23:08 -0700

    commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
  * main - tests function that print if integer is positive or negative
  * Reurn: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 98;
-	postive_or_negative(i);
+	positive_or_negative(i);
 
 	return (0);
+
 }
