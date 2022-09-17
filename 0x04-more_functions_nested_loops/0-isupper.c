c6017db3046c0711cf60a1b4870afc2ba5baccdf
victory <vicidundun@gmail.com>
2022-09-16 17:43:57 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,7 +10,7 @@ int _isupper(int c)
 	char uppercase = 'A';
 	int isupper = 0;
 
-	for (uppercase = 'A'; uppervase <= 'Z'; uppercase++)
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 	{
 		if (c == uppercase)
 		{
