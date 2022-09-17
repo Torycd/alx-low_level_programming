65b8af9929cb0e006b3bf3dec11275ea2e75cc46
victory <vicidundun@gmail.com>
2022-09-16 17:58:26 -0700

    commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,5 +7,17 @@
  */
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+	char i = '0';
+	int isdigit = 0;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == c)
+		{
+			isdigit = 1;
+			break;
+		}
+	}
+
+	return (isdigit);
 }
