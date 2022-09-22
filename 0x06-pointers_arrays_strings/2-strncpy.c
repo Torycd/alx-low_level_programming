9967920b69d929a0406ab4e4385b47600b81cfee
victory <vicidundun@gmail.com>
2022-09-22 10:07:41 -0700

    comit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
 /**
  * _strncpy - a function that copys the strings.
  * @dest: an input string
- * @src: an input
+ * @src: an input string
  * @n: an input integer
  * Return: A pointer to the resulting string
  */
