ba181a7decdc4f3650255cf85b035020c29185f9
victory <vicidundun@gmail.com>
2022-09-28 12:36:42 -0700

    commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,11 +3,11 @@
 /**
  * _strlen_recursion - a function that returns the length of a string.
  * @s: An input string to printing
- * Return: the length of the string
+ * Return: The length of the string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s)
-		return(1 + _strlen_recursions(s + 1));
+		return(1 + _strlen_recursion(s + 1));
 	return (0);
 }
