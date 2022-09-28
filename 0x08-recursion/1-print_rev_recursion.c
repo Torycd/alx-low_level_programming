347e5669598c6a536f52e6439eaae9573ebee754
victory <vicidundun@gmail.com>
2022-09-28 12:31:03 -0700

    commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _print_rev_recursion - a function that prints a string in reverse, 
+ * _print_rev_recursion - a function that prints a string in reverse,
  * followed by a new line.
  * @s: An input string to printing
  * Return: Nothing
  */
-void _print_rev_recursion(char *S)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
